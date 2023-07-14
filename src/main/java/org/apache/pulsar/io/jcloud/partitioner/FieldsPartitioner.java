ce94c93911189fccedfb866f76bffa9b6d41bd53
Ceyhun Can Ulker <ceyhun@intenseye.com>
2023-07-14 15:35:50 +0300

    Handle empty values in fields partitioning

--- src/main/java/org/apache/pulsar/io/jcloud/partitioner/FieldsPartitioner.java
+++ src/main/java/org/apache/pulsar/io/jcloud/partitioner/FieldsPartitioner.java
@@ -3,16 +3,17 @@ package org.apache.pulsar.io.jcloud.partitioner;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.pulsar.client.api.schema.GenericRecord;
 import org.apache.pulsar.functions.api.Record;
 import org.apache.pulsar.io.jcloud.BlobStoreAbstractConfig;
 
 /**
- * Partitions by configured list of field, creates a folder hierarchy in same order.
+ * Partitions by configured list of fields, creates a folder hierarchy in same order.
  *
  * @param <T> expected to be a GenericRecord
  */
-public class FieldsPartitioner<T> extends SimplePartitioner<T> {
+public class FieldsPartitioner<T> extends AbstractPartitioner<T> {
 
     private List<String> fields;
     private boolean ignoreMissing;
@@ -35,20 +36,23 @@ public class FieldsPartitioner<T> extends SimplePartitioner<T> {
                 .collect(Collectors.joining(PATH_SEPARATOR));
         } else {
             Class<?> cls = message.getClass();
-            throw new UnsupportedOperationException("Unknown record type class " + cls + ", expected GenericRecord!");
+            throw new IllegalArgumentException("Unknown record type class " + cls + ", expected GenericRecord!");
         }
 	}
 
     private String getFieldValue(GenericRecord genericRecord, String field) {
-        var value = genericRecord.getField(field);
-        if (value == null) {
-            if (ignoreMissing) {
-                return null;
-            } else {
-                throw new IllegalArgumentException("Field '" + field + "' not found in record");
-            }
+        Object value = genericRecord.getField(field);
+        String valueString = null;
+        if (value != null) {
+            valueString = value.toString().trim();
         }
-        return value.toString();
+        if (StringUtils.isBlank(valueString)) {
+            valueString = null;
+        }
+        if (!ignoreMissing && valueString == null) {
+            throw new IllegalArgumentException("Field '" + field + "' not found in record");
+        }
+        return valueString;
     }
 
 }
